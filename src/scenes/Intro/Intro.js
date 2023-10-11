5018aa742d77dfe35690150901f7345286569d01
TerevaM <terevamillet@yahoo.fr>
2023-10-11 10:23:29 +0200

    Intro done

---
+++ src/scenes/Intro/Intro.js
@@ -0,0 +1,25 @@
+import "./Intro.css";
+function Intro() {
+
+  //state
+
+  //comportement
+
+  //render
+
+  return (
+    <section>
+        <div id="title">
+        <h1>
+            Tereva Millet<span>Developpeur web</span>
+        </h1>
+        </div>
+        <div class="scroll_down">
+        <i class="arrow down" id="top_arrow"></i>
+        <i class="arrow down" id="bottom_arrow"></i>
+        </div>
+    </section>
+  );
+}
+
+export default Intro;
